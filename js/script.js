4583867249d81cca4d56d8bbf00ed18e23db1ea0
nishapathak13 <nish.pathak12@gmail.com>
2021-07-04 13:50:01 +0545

    landing page completed

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 var navItems = document.querySelector(".nav-items");
 var burger = document.querySelector(".hamburger");
 
+
 burger.addEventListener('click', function(){
     navItems.classList.toggle('open');
-    burger.classList.toggle('isactive')
+    burger.classList.toggle('isactive');
+    document.body.classList.toggle('nav-open')
 })
